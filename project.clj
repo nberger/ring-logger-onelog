d0109f3410d3c6e8249e8d025c7754582399a638
Nicolas Berger <nicoberger@gmail.com>
2015-09-21 19:48:18 -0300

    Version 0.7.1

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject ring-logger-onelog "0.7.1-SNAPSHOT"
+(defproject ring-logger-onelog "0.7.1"
   :description "OneLog implementation for ring-logger"
   :url "http://github.com/nberger/ring-logger-onelog"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring-logger "0.7.0"]
+                 [ring-logger "0.7.1"]
                  [onelog "0.4.5"]]
   :profiles {:dev {:dependencies [[ring/ring-mock "0.2.0"]]}})
