e15b6576ecd359220cf97065fe55cb6bd547be8e
Nicolas Berger <nicoberger@gmail.com>
2016-03-26 14:59:27 -0300

    Bump to 0.7.5.1-SNAPSHOT
    This is so we can release a new version that is still aligned with
    ring-logger 0.7.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-logger-onelog "0.7.6-SNAPSHOT"
+(defproject ring-logger-onelog "0.7.5.1-SNAPSHOT"
   :description "OneLog implementation for ring-logger"
   :url "http://github.com/nberger/ring-logger-onelog"
   :license {:name "Eclipse Public License"
