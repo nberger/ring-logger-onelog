52cbcc705f47be56775790b7c5ddcb296e990ff4
Nicolas Berger <nicoberger@gmail.com>
2016-03-29 22:04:32 -0300

    Version 0.7.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-logger-onelog "0.7.7-SNAPSHOT"
+(defproject ring-logger-onelog "0.7.7"
   :description "OneLog implementation for ring-logger"
   :url "http://github.com/nberger/ring-logger-onelog"
   :license {:name "Eclipse Public License"
